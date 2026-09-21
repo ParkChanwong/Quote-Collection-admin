6dc6a8012aff506eba9e3f58a87896c89bc00050
parkchanwong <cksdnd5433@gmail.com>
2026-09-21 15:45:44 +0900

    feat: add quote page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Layout from './layouts';
+import Quote from './pages/Quote';
 
 const App = () => {
 
   return (
     <Routes>
       <Route path='/login' element={ <Login /> } />
+      <Route element={<Layout />}>
+        <Route index element={<Navigate to='/quotes' replace />} />
+        <Route path='/quotes' element={<Quote />} />
+      </Route>
     </Routes>
   )
 }
