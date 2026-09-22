5dc81e8eb018e258f398d7f44a6a0f5117f9f7a4
parkchanwong <cksdnd5433@gmail.com>
2026-09-22 14:34:01 +0900

    fix: 서버 배포 url 로 변경

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const TOKEN_KEY = 'quote-admin-token';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://quote-collection-odpj.onrender.com',
     timeout: 10000
 });
 
