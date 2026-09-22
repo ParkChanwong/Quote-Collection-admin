66f0fcac6415e504ca23e0438d5419fdd8ccd8ee
parkchanwong <cksdnd5433@gmail.com>
2026-09-22 09:38:05 +0900

    feat: add react query client connection

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './api/queryClient'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
@@ -6,8 +8,10 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
